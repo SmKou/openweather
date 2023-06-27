3b510b75c2b6440f7d8432166fd293790dbe1a0b
SmKou <66178856+SmKou@users.noreply.github.com>
2023-06-27 09:52:16 -0700

    Refactor http request to promise

--- src/js/bs/index-no-promise.js
+++ src/js/bs/index-no-promise.js
@@ -1,31 +1,16 @@
-const form = {
-    elem: document.querySelector('form'),
-    loc: document.querySelector('#location'),
-    zip: document.querySelector('#zipcode'),
-    res: document.querySelector('#showResponse')
-};
-
-const data = {
-    temp_setting: '',
-    city: '',
-    zipcode: ''
-};
-
 function getWeather() {
-    const promise = new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        const url = `http://api.openweathermap.org/data/2.5/weather?${ data.city ? 'q=' + data.city : 'zip=' + data.zipcode }&appid=${process.env.API_KEY}${ data.temp_setting !== 'K' ? '&units=' + getTempType(data.temp_setting) : '' }`;
-        request.addEventListener('loadend', function () {
-            const resp = JSON.parse(this.responseText);
-            if (this.status === 200)
-                resolve(resp);
-            else
-                reject(this);
-        });
-        request.open("GET", url, true);
-        request.send();
+    const request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?${ data.city ? 'q=' + data.city : 'zip=' + data.zipcode }&appid=${process.env.API_KEY}${ data.temp_setting !== 'K' ? '&units=' + getTempType(data.temp_setting) : '' }`;
+    request.addEventListener('loadend', function () {
+        const resp = JSON.parse(this.responseText);
+        alert(resp.sys.sunrise + ' and ' + resp.sys.sunset);
+        if (this.status === 200)
+            printData(resp);
+        else
+            printError(resp);
     });
-    promise.then(printData, printError);
+    request.open("GET", url, true);
+    request.send();
 }
 
 function printData(resp) {
